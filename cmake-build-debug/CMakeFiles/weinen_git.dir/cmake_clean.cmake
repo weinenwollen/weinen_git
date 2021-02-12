160a9d07544146e594245924c5d023556f28bc31
weinenwollen <weinenwollen@localhost.localdomain>
2021-02-12 11:01:45 +0300

    Добавлен printf Axe

---
+++ cmake-build-debug/CMakeFiles/weinen_git.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/weinen_git.dir/main.cpp.o"
+  "weinen_git"
+  "weinen_git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/weinen_git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ qwerty.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("give me a bottle of rum!\n");
+	return 0;
+}
